3aa8444842b83feb0a524a004a3b78e29bd39597
toastsandwich <shreyasxcore2003@gmail.com>
2025-02-02 09:07:14 +0000

    updated userspace code

--- go/userspace.go
+++ go/userspace.go
@@ -57,38 +57,52 @@ func main() {
 	seen := make(map[uint32]uint64)
 	mu := sync.Mutex{}
 	go func() {
+		ticker := time.NewTicker(500 * time.Millisecond)
+		defer ticker.Stop()
 		for {
-			iter := xdp_map.Iterate()
-			var key uint32
-			var val uint64
+			select {
+			case <-ticker.C:
+				PrintStat(xdp_map, &mu, seen)
+			case <-stop:
+				return
 
-			fmt.Print("\033[H\033[J")
-			mu.Lock()
-
-			updated := false
-			for iter.Next(&key, &val) {
-				seen[key] = val
-				updated = true
-			}
-
-			for k, v := range seen {
-				ip := GetIP(k)
-				names, err := net.LookupAddr(ip)
-				if err != nil {
-					fmt.Printf("%s", err.Error())
-					continue
-				}
-				fmt.Printf("%v: %d\n", names, v)
-			}
-
-			mu.Unlock()
-			if updated {
-				time.Sleep(500 * time.Millisecond)
 			}
 		}
 	}()
 	<-stop
 	fmt.Println("detaching program..")
+}
+
+func PrintStat(xdp_map *ebpf.Map, mu *sync.Mutex, seen map[uint32]uint64) {
+	for {
+		iter := xdp_map.Iterate()
+		var key uint32
+		var val uint64
+
+		fmt.Print("\033[H\033[J")
+		mu.Lock()
+
+		updated := false
+		for iter.Next(&key, &val) {
+			seen[key] = val
+			updated = true
+		}
+
+		for k, v := range seen {
+			ip := GetIP(k)
+			names, err := net.LookupAddr(ip)
+			if err != nil {
+				fmt.Printf("%s\n", err.Error())
+				continue
+			}
+			fmt.Printf("\r%v: %d\n", names, v)
+		}
+
+		mu.Unlock()
+		if updated {
+			time.Sleep(500 * time.Millisecond)
+		}
+	}
 }
 
 func GetIP(ip uint32) string {
